c19bace0d87e963f332b56183ddb62720e401058
Jarrod Watts <35651410+jarrodwatts@users.noreply.github.com>
2022-08-03 07:23:50 +1000

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: {
+    version: '0.8.9',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
 
 export default config;
